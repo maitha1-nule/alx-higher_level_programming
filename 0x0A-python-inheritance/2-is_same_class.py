751b80c90fa352082cf8266b368462f7ff81bf34
maitha1-nule <nuleahmed6@gmail.com>
2024-01-08 23:50:34 +0300

    returns true if it is an istance of the specified class else false

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """this function returns true if objects is an instance else false"""
 
+
 def is_same_class(obj, a_class):
     """this function will be used to check if it is an instance"""
     return type(obj) is a_class
