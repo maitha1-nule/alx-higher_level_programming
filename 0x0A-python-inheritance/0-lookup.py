740ebbfad3a61be7d010aa81bc6fc0329e572e71
maitha1-nule <nuleahmed6@gmail.com>
2024-01-08 22:26:27 +0300

    In this first task we was implementing a function that would be used to  return the list of all  available methods and attributes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""Used to define an attribute(obj) that belongs to the lookup function"""
+
 def lookup(obj):
+"""Returns a ist of available attributes of an object"""
     return dir(obj)
