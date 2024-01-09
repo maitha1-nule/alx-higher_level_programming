5b0c8cfd9d3e3356666568de1bb2c6f29b69df8d
maitha1-nule <nuleahmed6@gmail.com>
2024-01-10 01:11:07 +0300

    A function that writes to a text and returns the numbers of characters printed

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -12,5 +12,5 @@ def write_file(filename="", text=""):
     Returns:
         The number of characters written
     """
-    with open(filename, 'w', encoding='utf-8') as file:
+    with open(filename, "w", encoding="utf-8") as file:
         return f.write(text)
