fe52360f7a4bc46ab74154a248c578265ddc07dd
maitha1-nule <nuleahmed6@gmail.com>
2024-01-10 00:24:13 +0300

    A function that reads a text and prints it out

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,3 +12,5 @@ def read_file(filename=""):
         with open(filename, 'r', encoding='utf-8') as file:
             for line in file:
                 print(line, end='')
+    except Exception as e:
+        print(f"Error reading the file: {e}")
