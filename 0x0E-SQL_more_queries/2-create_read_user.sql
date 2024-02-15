dfe614cc47f87b59e54e68e407437c1285b9e9e2
maitha1-nule <nuleahmed@gmail.com>
2024-02-16 02:51:33 +0300

    the script creates a table and "id, name = (INT, VARCHAR (256) NOT NULL) repsectively

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED WITH 'user_0d_2_pwd';
-GRANT USER SELECT ON  hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT USER SELECT hbtn_0d_2.* TO 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+--A script that creates the table_name on mysql database
+--Attribute1: int
+--Attribute2: name VARCHAT(256)
+--if sript is avalilable, retun no error!!!
+
+CREATE TABLE IF NOT EXISTS 'force_name' ('id' INT, 'name' VARCHAR(256) NOT NULL);
