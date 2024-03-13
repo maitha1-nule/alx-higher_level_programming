86b4388e1e8028ad4ad73adb3e2d568ee20a0257
maitha1-nule <nuleahmed@gmail.com>
2024-03-13 04:40:28 +0300

    In this task ive implemented inhertitance on the new square  on which ive created a new instance method that prints a rectangle using the characterc if nit defined then X

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 const Rectangle = require('./5-square.js');
 class Square extends Rectangle {
-charPrint(c) {
-if ( c === undefined ) {
-c = 'X';
-}
-for (let i = 0; i < this.height; i++) {
-if (i < this.width) {
-console.log(c.repeat(this.width));
-} else {
-console.log(c.repeat(this.width));
-}
-}
-}
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      if (i < this.width) {
+        console.log(c.repeat(this.width));
+      } else {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
 }
 module.exports = Square;
