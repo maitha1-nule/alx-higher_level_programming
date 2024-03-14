69a550234c8ce5ab0e40c03549940bb85dd8eca1
maitha1-nule <nuleahmed@gmail.com>
2024-03-14 16:26:02 +0300

    in this task i've come up with a script which displays the number of records matched using a while statement

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- This script displays a number of records with id =89
 -- in the table firts_table
-SELECT COUNT (*) FROM first_table WHERE id = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
