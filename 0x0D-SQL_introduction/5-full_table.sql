115211cc73bf9a94fcf1d7437888f1b53ef980d4
maitha1-nule <nuleahmed@gmail.com>
2024-03-14 16:02:11 +0300

    In this task cerated a script that prints the full description of the table firts table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
----this script will be printing the full descrpion of a table
---without describe or explain
-
-
-SHOW CREATE TABLE 'first_table';
+-- this script will be printing the full descrpion of a table
+-- without describe or explain
+SHOW CREATE TABLE first_table;
