87bd736149a7386dabcc6591e570a3e2748b7ac2
maitha1-nule <nuleahmed@gmail.com>
2024-03-14 16:31:13 +0300

    In this task ive created a second table and added some data to it

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
---A script that creates a table named second_table
---in the databse hbtn_0c_c
+-- A script that creates a table named second_table
+-- in the databse hbtn_0c_c
 
 
-CREATE TABLE IF NOT EXISTS 'second_table'( 'id' INT, 'name' VARCHAR(256), 'score' INT);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES(1,  'John', 'score' = 10);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES(2,  'Alex', 'score' = 3);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES(3,  'Bob', 'score' = 14);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES(4,  'George', 'score' = 8);
+CREATE TABLE IF NOT EXISTS second_table( id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES(1,  John, score = 10);
+INSERT INTO second_table (id, name, score) VALUES(2,  Alex, score = 3);
+INSERT INTO second_table (id, name, score) VALUES(3,  'Bob', score = 14);
+INSERT INTO second_table (id, name, score) VALUES(4,  'George', score = 8);
