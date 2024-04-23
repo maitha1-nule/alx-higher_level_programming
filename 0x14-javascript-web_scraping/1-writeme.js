293eb7de8294ca7dee9fc36053be1934503c9995
maitha1-nule <nuleahmed@gmail.com>
2024-04-24 02:06:59 +0300

    In this task i've come up with a script which writes to a file and changed some failing semistandard requirements

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,10 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
-fs.writeFile(filePath, content, { encoding: 'utf-8'}, (err) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log(content);
+fs.writeFile(filePath, content, { encoding: 'utf-8' }, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(content);
 });
